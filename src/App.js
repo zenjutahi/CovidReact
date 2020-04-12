c96e694192adce25fb6d5c8390b6ffbd8203f4ab
zenjutahi <zenjutahi@gmail.com>
2020-04-12 11:08:42 +0300

    [feature] - add current time

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './assets/main.css';
 import Header from './components/header/header.component';
-import Global from './components/global/global.component';
-import Directory from './components/directory/directory.component';
+import Wrapper from './components/wrapper/wrapper.component';
 import './App.css';
 
 function App() {
@@ -10,32 +9,7 @@ function App() {
   return (
     <div className="App">
         <Header />
-        <div className="page-container">
-
-            <div className="page-content">
-
-
-                <div className="page-content-wrap">
-
-                    <div className="row">
-                        <div className="col-md-12">
-
-                            <div className="panel panel-default">
-                                <div className="panel-heading">
-                                    <h3 className="panel-title"><span class="fa fa-bar-chart-o"></span> COVID TRACKER <small> Keep yourself Updated</small></h3>
-                                </div>
-                                <div className="panel-body">
-                                <Global />
-                                <Directory />
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
+        <Wrapper />
     </div>
   );
 }
