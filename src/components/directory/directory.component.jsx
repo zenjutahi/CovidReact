ddf3c8962882253456b82b75e2bd7672bd523196
zenjutahi <zenjutahi@gmail.com>
2020-04-12 11:08:52 +0300

    [feature] - add current time

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+// import { Switch, Route } from 'react-router-dom';
 import CollectionItem from '../collection-item/collection-item.component';
 
 import SearchBox from '../search-box/search-box.component';
